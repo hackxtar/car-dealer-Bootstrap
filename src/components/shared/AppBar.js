dee38467ddfcb7af983f818dc0ebaa422dca4e83
Saif Ali <hackxtar@gmail.com>
2022-07-03 15:56:10 +0530

    Nav Bar

--- src/components/shared/AppBar.js
+++ src/components/shared/AppBar.js
@@ -2,48 +2,67 @@ import React from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import "./AppBar.scss";
 
-const Logo = () => {
-  return (
-    <div className="navbar-logo">
-      <span>CAR</span>
-      <b>DEALER</b>
-    </div>
-  );
-};
-
-const Menu = () => {
-  return (
-    <div className="float-right navbar-menu">
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </div>
-  );
-};
-
 const AppBar = () => {
   return (
-    <Navbar expand="lg">
-      <Container>
-        <Logo />
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Menu />
-      </Container>
-    </Navbar>
+    <header class="header-area header-sticky">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <nav class="main-nav">
+              <a href="index.html" class="logo">
+                Car Dealer<em> Website</em>
+              </a>
+              <ul class="nav">
+                <li>
+                  <a href="index.html" class="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="cars.html">Cars</a>
+                </li>
+                <li class="dropdown">
+                  <a
+                    class="dropdown-toggle"
+                    data-toggle="dropdown"
+                    href="#"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    About
+                  </a>
+
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="about.html">
+                      About Us
+                    </a>
+                    <a class="dropdown-item" href="blog.html">
+                      Blog
+                    </a>
+                    <a class="dropdown-item" href="team.html">
+                      Team
+                    </a>
+                    <a class="dropdown-item" href="testimonials.html">
+                      Testimonials
+                    </a>
+                    <a class="dropdown-item" href="faq.html">
+                      FAQ
+                    </a>
+                    <a class="dropdown-item" href="terms.html">
+                      Terms
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <a href="contact.html">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 

0b78957c75ed637c3456ddbaf96ddd23e8975b80
Saif Ali <hackxtar@gmail.com>
2022-07-05 10:18:12 +0530

    Additional Styling Home Featured Cars

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -30,7 +30,7 @@ const FeaturedCars = () => {
         </h1>
       </Row>
       <Row className="my-5 justify-content-center">
-        {[1, 2, 3, 4, 5, 6].map((e) => (
+        {[1, 2, 3].map((e) => (
           <FeaturedCarsSingle />
         ))}
       </Row>
@@ -40,20 +40,38 @@ const FeaturedCars = () => {
 
 const FeaturedCarsSingle = () => {
   return (
-    <Card style={{ width: "20rem" }} className="m-4">
-      <Card.Img
-        variant="top"
-        src="https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80/100px180"
-      />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button className="bg-primary">View</Button>
-      </Card.Body>
-    </Card>
+    <Col className="lg-4">
+      <div class="trainer-item">
+        <div class="image-thumb">
+          <img
+            src="https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80/100px180"
+            alt=""
+          />
+        </div>
+        <div class="down-content">
+          <span>
+            <del>
+              <sup>$</sup>11999
+            </del>
+            &nbsp; <sup>$</sup>11779
+          </span>
+
+          <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+
+          <p>
+            <i class="fa fa-dashboard"></i> 130 000km &nbsp;&nbsp;&nbsp;
+            <i class="fa fa-cube"></i> 1800 cc &nbsp;&nbsp;&nbsp;
+            <i class="fa fa-cog"></i> Manual &nbsp;&nbsp;&nbsp;
+          </p>
+
+          <ul class="social-icons list-style-none">
+            <li>
+              <a href="car-details.html">+ View Car</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Col>
   );
 };
 

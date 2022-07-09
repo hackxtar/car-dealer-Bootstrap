b3490b8dd14bea5dea5406e03d056defdb7a62d4
Saif Ali <hackxtar@gmail.com>
2022-07-10 01:37:31 +0530

    Footer Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import AppBar from "./components/shared/AppBar";
 import Home from "./components/home/Home";
 import "./App.scss";
+import Footer from "./components/shared/footer/Footer";
 
 function App() {
   return (
     <>
       <AppBar />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/shared/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <p>
+              Copyright © 2022 Company Name -
+              <a href="#carFooter">Car Dealers</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
